4e8ef4eb1c9368297297b291345d95b19c3b01f7
hualuozhe <hualuozhe@gmail.com>
2019-06-27 10:34:25 +0800

    init

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,28 @@
+<template>
+<div>
+
+<qHeader></qHeader>
+
+</div>
+</template>
+
+<script>
+
+import qHeader from './components/Header.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    qHeader
+  }
+}
+</script>
+
+
+<style lang="less" scoped>
+
+h1 {
+  color: red;
+}
+
+</style>
---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+
+<h2>headers</h2>
+
+</div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
+
